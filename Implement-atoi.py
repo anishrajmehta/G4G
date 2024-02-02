07a7f607d9159e6d76da85fa6c8d425f6d10a550
anishraj mehta <anishrajmehta@icloud.com>
2024-02-02 10:36:44 +0530

    Create Implement-atoi.py

---
+++ Implement-atoi.py
@@ -0,0 +1,72 @@
+"""
+
+2 February 2024
+
+Given a string, s, the objective is to convert it into integer format without utilizing any built-in functions. If the conversion is not feasible, the function should return -1.
+
+Note: Conversion is feasible only if all characters in the string are numeric or if its first character is '-' and rest are numeric.
+
+Example 1:
+
+Input:
+s = "-123"
+Output: 
+-123
+Explanation:
+It is possible to convert -123 into an integer 
+and is so returned in the form of an integer
+Example 2:
+
+Input:
+s = "21a"
+Output: 
+-1
+Explanation: 
+The output is -1 as, due to the inclusion of 'a',
+the given string cannot be converted to an integer.
+Your Task:
+You do not have to take any input or print anything. Complete the function atoi() which takes a string s as an input parameter and returns an integer value representing the given string. If the conversion is not feasible, the function should return -1.
+
+|s| = length of string str.
+Expected Time Complexity: O( |s| ), 
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ |s| ≤ 10
+
+"""
+#User function template for Python
+
+class Solution:
+    # your task is to complete this function
+    # function should return an integer
+    def atoi(self,s):
+        # Code here
+        s = s.strip()
+        
+        sign = 1
+        if s[0] == '-':
+            sign = -1
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+            
+        num = 0
+        for char in s:
+            if not char.isdigit():
+                return -1
+            num = num * 10 + (ord(char) - ord('0'))
+            
+        return sign * num
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        string = input().strip();
+        ob=Solution()
+        print(ob.atoi(string))
+# } Driver Code Ends
