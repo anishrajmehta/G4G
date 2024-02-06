5cab946fdcc0da93ca660c32f70653f66c374562
anishraj mehta <anishrajmehta@icloud.com>
2024-02-06 22:24:53 +0530

    Create count-the-nodes-at-distance-k-from-leaf.cpp

---
+++ count-the-nodes-at-distance-k-from-leaf.cpp
@@ -0,0 +1,204 @@
+/*
+
+6 February 2024
+
+Given a binary tree with n nodes and a non-negative integer k, the task is to count the number of special nodes.
+A node is considered special if there exists at least one leaf in its subtree such that the distance between the node and leaf is exactly k.
+
+Note: Any such node should be counted only once. For example, if a node is at a distance k from 2 or more leaf nodes, then it would add only 1 to our count.
+
+Example 1:
+
+Input:
+Binary tree
+        1
+      /   \
+     2     3
+   /  \   /  \
+  4   5  6    7
+          \ 
+          8
+k = 2
+Output: 
+2
+Explanation: 
+There are only two unique nodes that are at a distance of 2 units from the leaf node. (node 3 for leaf with value 8 and node 1 for leaves with values 4, 5 and 7) Note that node 2 isn't considered for leaf with value 8 because it isn't a direct ancestor of node 8.
+Example 2:
+
+Input:
+Binary tree
+          1
+         /
+        3
+       /
+      5
+    /  \
+   7    8
+         \
+          9
+k = 4
+Output: 
+1
+Explanation: 
+Only one node is there which is at a distance of 4 units from the leaf node.(node 1 for leaf with value 9) 
+Your Task:
+You don't have to read input or print anything. Complete the function printKDistantfromLeaf() that takes root node and k as inputs and returns the number of nodes that are at distance k from a leaf node. 
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= n <= 105
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Node Structure
+/*struct NodeGiven a binary tree with n nodes and a non-negative integer k, the task is to count the number of special nodes. A node is considered special if there exists at least one leaf in its subtree such that the distance between the node and leaf is exactly k.
+{
+	int data;
+	Node *left, *right;
+};*/
+
+class Solution {
+    int count;
+    void countNodes(Node* node, int k, int path[], bool visited[], int level) {
+        if (node == NULL) return;
+
+        path[level] = node->data;
+        visited[level] = false;
+
+        if (node->left == NULL && node->right == NULL && level - k >= 0 && visited[level - k] == false) {
+            count++;
+            visited[level - k] = true;
+            return;
+        }
+
+        countNodes(node->left, k, path, visited, level + 1);
+        countNodes(node->right, k, path, visited, level + 1);
+    }
+
+public:
+    int printKDistantfromLeaf(Node* root, int k) {
+        int path[MAX_HEIGHT];
+        bool visited[MAX_HEIGHT] = {false};
+        count = 0;
+        countNodes(root, k, path, visited, 0);
+        return count;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s, ks;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, ks);
+        int k=stoi(ks);
+        Solution obj;
+        cout<<obj.printKDistantfromLeaf(root, k)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
